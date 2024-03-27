daf86d00eaf58afffa3cac53ffefa1eaf8d27c0e
shackgerc <sagerprogramer@gmail.com>
2024-03-27 14:44:49 -0300

    First commit

---
+++ src/main/java/org/example/rallyroundbackend/RallyRoundBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.example.rallyroundbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RallyRoundBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RallyRoundBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/rallyroundbackend/RallyRoundBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.rallyroundbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RallyRoundBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
