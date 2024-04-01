8cc2f1903c8c0196c84fc4bb36655c4f6af07675
shackgerc <sagerprogramer@gmail.com>
2024-04-01 18:56:32 -0300

    Add a utility to compress and decompress images.

---
+++ src/main/java/org/fs/rallyroundbackend/util/ImageUtil.java
@@ -0,0 +1,41 @@
+package org.fs.rallyroundbackend.util;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
+
+public class ImageUtil {
+    public static byte[] compressImage(byte[] data) throws IOException {
+
+        Deflater deflater = new Deflater();
+        deflater.setLevel(4);
+        deflater.setInput(data);
+        deflater.finish();
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+        byte[] buffer = new byte[4 * 1024];
+
+        int size = deflater.deflate(buffer);
+        outputStream.write(buffer, 0, size);
+        outputStream.close();
+
+        return outputStream.toByteArray();
+    }
+
+    public static byte[] decompressImage(byte[] data) throws DataFormatException, IOException {
+        Inflater inflater = new Inflater();
+        inflater.setInput(data);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+
+        byte[] buffer = new byte[4 * 1024];
+
+        while (!inflater.finished()) {
+            int count = inflater.inflate(buffer);
+            outputStream.write(buffer, 0, count);
+        }
+        outputStream.close();
+        return outputStream.toByteArray();
+    }
+}
