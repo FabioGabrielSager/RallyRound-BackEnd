a3c2e9ec4365fd326c680936a59a10b6189b09b3
shackgerc <sagerprogramer@gmail.com>
2024-04-06 16:38:49 -0300

    Change locations dtos to make the process of validating places easier.
    Add a method to PlaceDto to get the formated name to use it as a query in a autosuggestion places request to the Bing maps API.
    Overwrite equals method in placeDto.
    Overwrite equals method in AddressDto.
    Optimize Jakarta validation annotations usage in DTOs.

--- src/main/java/org/fs/rallyroundbackend/dto/location/Address.java
+++ src/main/java/org/fs/rallyroundbackend/dto/location/Address.java
@@ -1,29 +1,51 @@
 package org.fs.rallyroundbackend.dto.location;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.Objects;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Address {
     @NotBlank
-    @NotNull
     private String adminDistrict;
-    @NotBlank @NotNull
+    @NotBlank
     private String adminDistrict2;
-    @NotBlank @NotNull
+    @NotBlank
     private String locality;
-    @NotBlank @NotNull
+    @NotBlank
     private String countryRegion;
-    @NotBlank @NotNull
+    @NotBlank
     private String formattedAddress;
     private String addressLine;
     private String postalCode;
     private String neighborhood;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Address) {
+            Address address = (Address) obj;
+
+            return Objects.equals(this.adminDistrict, address.adminDistrict)
+                    && Objects.equals(this.adminDistrict2, address.adminDistrict2)
+                    && Objects.equals(this.locality, address.locality)
+                    && Objects.equals(this.countryRegion, address.countryRegion)
+                    && Objects.equals(this.formattedAddress, address.formattedAddress)
+                    && Objects.equals(this.addressLine, address.addressLine)
+                    && Objects.equals(this.postalCode, address.postalCode)
+                    && Objects.equals(this.neighborhood, address.neighborhood);
+        }
+        return false;
+    }
 }
---
+++ src/main/java/org/fs/rallyroundbackend/dto/location/PlaceDto.java
@@ -0,0 +1,64 @@
+package org.fs.rallyroundbackend.dto.location;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PlaceDto {
+    @JsonProperty("__type")
+    @NotBlank @NotNull
+    private String entityType;
+    @NotNull
+    private Address address;
+    private String name;
+
+    public String getFormattedName() {
+        String formattedName = "";
+
+        if(this.address != null)  {
+            if(this.address.getAdminDistrict() != null && !this.address.getAdminDistrict().isEmpty()) {
+                formattedName += this.address.getAdminDistrict() + ", ";
+            }
+            if(this.address.getAdminDistrict2() != null && !this.address.getAdminDistrict2().isEmpty()) {
+                formattedName += this.address.getAdminDistrict2() + ", ";
+            }
+            if(this.address.getLocality() != null && !this.address.getLocality().isEmpty()) {
+                formattedName += this.address.getLocality() + ", ";
+            }
+            if(this.address.getNeighborhood() != null && !this.address.getNeighborhood().isEmpty()) {
+                formattedName += this.address.getNeighborhood() + ", ";
+            }
+            if(this.name != null && !this.name.isEmpty()) {
+                formattedName += this.name;
+            }
+        }
+
+        return formattedName.endsWith(", ") ? formattedName.substring(0, formattedName.lastIndexOf(", "))
+                : formattedName;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof PlaceDto) {
+
+            PlaceDto placeDto = (PlaceDto) obj;
+
+            return Objects.equals(this.entityType, placeDto.entityType)
+                    && Objects.equals(this.name, placeDto.name)
+                    && Objects.equals(this.address, placeDto.address);
+        }
+
+        return false;
+    }
+}
