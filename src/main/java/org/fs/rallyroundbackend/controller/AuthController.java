6d55f122f7fd3d95e632eca265f6ea6e94dd371d
shackgerc <sagerprogramer@gmail.com>
2024-04-06 16:35:12 -0300

    Change the registerParticipant configuration to be able to receive a profile photo as a argument of the post.

--- src/main/java/org/fs/rallyroundbackend/controller/AuthController.java
+++ src/main/java/org/fs/rallyroundbackend/controller/AuthController.java
@@ -1,5 +1,7 @@
 package org.fs.rallyroundbackend.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.fs.rallyroundbackend.dto.auth.AuthResponse;
@@ -9,13 +11,18 @@ import org.fs.rallyroundbackend.dto.auth.ParticipantRegistrationRequest;
 import org.fs.rallyroundbackend.dto.auth.ParticipantRegistrationResponse;
 import org.fs.rallyroundbackend.exception.UnsuccefulyEmailVerificationException;
 import org.fs.rallyroundbackend.service.AuthService;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.print.attribute.standard.Media;
 import java.util.Locale;
 
 @RestController
@@ -23,23 +30,31 @@ import java.util.Locale;
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
+    private final ObjectMapper objectMapper;
 
     @PostMapping(value = "/participant/login")
     public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
         return ResponseEntity.ok(authService.login(request));
     }
 
-    @PostMapping(value = "/participant/register")
+    @PostMapping(value = "/participant/register",
+    consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
     public ResponseEntity<ParticipantRegistrationResponse>
-    registerParticipant(@RequestBody @Validated ParticipantRegistrationRequest request, Locale locale) {
-        ParticipantRegistrationResponse response = authService.registerParticipant(request, locale);
+    registerParticipant(@RequestPart @Validated String participantData,
+                        @RequestParam(required = false) MultipartFile profilePhoto, Locale locale)
+            throws JsonProcessingException {
+
+        ParticipantRegistrationRequest participantDataPojo = objectMapper.readValue(participantData,
+                ParticipantRegistrationRequest.class);
+
+        ParticipantRegistrationResponse response =
+                authService.registerParticipant(participantDataPojo, profilePhoto, locale);
         return ResponseEntity.ok(response);
     }
 
     @PostMapping(value = "/participant/confirm/email")
     public ResponseEntity<AuthResponse> completeRegistration(@RequestBody @Validated ConfirmParticipantRegistrationRequest
-                                                                     confirmParticipantRegistrationRequest)
-            throws UnsuccefulyEmailVerificationException {
+                                                                     confirmParticipantRegistrationRequest) {
         return ResponseEntity.ok(
                 this.authService.confirmParticipantRegistration(confirmParticipantRegistrationRequest)
         );
