e71e223e739501cb516450dcbafcde229a87a0b5
shackgerc <sagerprogramer@gmail.com>
2024-04-23 15:58:31 -0300

    Add new mocked user with an associated mercado pago test account.

--- src/main/resources/devdata.sql
+++ src/main/resources/devdata.sql
@@ -18,4 +18,25 @@ values ('a1b1488c-b222-4a01-b88e-7dc7ce7a58f8', 'mock', 'mocked', 'mockUser@emai
 
 insert into users_roles (user_id, role_id) values ('a1b1488c-b222-4a01-b88e-7dc7ce7a58f8', 2);
 
-insert into participants(id, reputation) values ( 'a1b1488c-b222-4a01-b88e-7dc7ce7a58f8', 'BUENA' )
+insert into participants(id, reputation) values ( 'a1b1488c-b222-4a01-b88e-7dc7ce7a58f8', 'BUENA' );
+
+-- Mocked participant user with liked mercado pago account. Password=mockpass
+insert into users (id, name, last_name, email, birthdate, password, account_non_expired,
+                   credentials_non_expired, account_non_locked, enabled)
+
+values ('51ef4c13-ccbc-4054-904b-6f70e436f7d5', 'mpuser', 'mpuser', 'mpuser@email.com', '2001-10-31',
+        '$2a$12$TRXJRcBWduilWn.VDonMk.EsLADjol79NBr0mDR.yaq3l13bIkmUu',
+        true, true, true, true);
+
+insert into users_roles (user_id, role_id) values ('51ef4c13-ccbc-4054-904b-6f70e436f7d5', 2);
+
+insert into participants(id, reputation) values ( '51ef4c13-ccbc-4054-904b-6f70e436f7d5', 'BUENA' );
+
+insert into mp_auth_tokens(id, access_token, token_type, expire_in, user_code, scope, refresh_token, public_key)
+values ('51ef4c13-ccbc-4054-904b-6f70e436f7d5',
+        'APP_USR-7019845672515236-032609-73133eb62337f22abdb6612e0f34c262-1744739090',
+        'bearer', 15552000, 1744739090, 'offline_access read write', 'TG-XXXXXXXX-241983636',
+        'APP_USR-8aa919b3-634f-4ab7-af5f-51087dbd18f5');
+
+update participants set mp_auth_token_id='51ef4c13-ccbc-4054-904b-6f70e436f7d5'
+                    where id='51ef4c13-ccbc-4054-904b-6f70e436f7d5';
