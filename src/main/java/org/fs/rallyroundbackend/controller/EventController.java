23c08c9b3bd6e0eba07242bbb393c19ce64921cb
shackgerc <sagerprogramer@gmail.com>
2024-04-23 17:10:14 -0300

    Add EventController with endpoint to create an event and its security configuration.

---
+++ src/main/java/org/fs/rallyroundbackend/controller/EventController.java
@@ -0,0 +1,30 @@
+package org.fs.rallyroundbackend.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.fs.rallyroundbackend.dto.event.EventDto;
+import org.fs.rallyroundbackend.service.EventService;
+import org.fs.rallyroundbackend.service.JwtService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rr/api/v1/events")
+@RequiredArgsConstructor
+public class EventController {
+
+    private final EventService eventService;
+    private final JwtService jwtService;
+
+    @PostMapping("/create/")
+    public ResponseEntity<EventDto> createEvent(@RequestBody EventDto eventDto,
+                                                HttpServletRequest request) {
+        
+        String creatorEmail = jwtService.getUsernameFromToken(jwtService.getTokenFromRequest(request));
+
+        return ResponseEntity.ok(this.eventService.createEvent(eventDto, creatorEmail));
+    }
+}
