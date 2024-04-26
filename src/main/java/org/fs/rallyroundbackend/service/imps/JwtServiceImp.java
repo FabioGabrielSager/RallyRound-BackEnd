4ea47dc9114d9103fdc1f359391da9ed389454f2
shackgerc <sagerprogramer@gmail.com>
2024-04-26 10:16:26 -0300

    Fix generated JWT expiration date.

--- src/main/java/org/fs/rallyroundbackend/service/imps/JwtServiceImp.java
+++ src/main/java/org/fs/rallyroundbackend/service/imps/JwtServiceImp.java
@@ -35,7 +35,7 @@ public class JwtServiceImp implements JwtService {
                 .claims(extraClaims)
                 .subject(user.getUsername())
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis()+1000*60*24))
+                .expiration(new Date(System.currentTimeMillis()+1000*60*60*24))
                 .signWith(getKey())
                 .compact();
     }
