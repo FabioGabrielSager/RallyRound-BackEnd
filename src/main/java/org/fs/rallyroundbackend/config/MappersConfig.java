ae7c1d436509be8864bf2d0da9fcc3aa660c97e9
shackgerc <sagerprogramer@gmail.com>
2024-04-26 10:05:59 -0300

    Add custom converter to ModelMapper bean definition to map EventParticipantEntity objects to EventParticipantResponse objects.

--- src/main/java/org/fs/rallyroundbackend/config/MappersConfig.java
+++ src/main/java/org/fs/rallyroundbackend/config/MappersConfig.java
@@ -2,16 +2,45 @@ package org.fs.rallyroundbackend.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.fs.rallyroundbackend.dto.event.EventParticipantResponse;
+import org.fs.rallyroundbackend.dto.participant.ParticipantResume;
+import org.fs.rallyroundbackend.entity.events.EventParticipantEntity;
+import org.modelmapper.AbstractConverter;
 import org.modelmapper.Conditions;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Base64;
+
 @Configuration
 public class MappersConfig {
 
     @Bean
-    public ModelMapper modelMapper() { return new ModelMapper(); }
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.addConverter(new AbstractConverter<EventParticipantEntity, EventParticipantResponse>() {
+            @Override
+            protected EventParticipantResponse convert(EventParticipantEntity source) {
+                ParticipantResume participantNameAndProfilePhoto = new
+                        ParticipantResume();
+                participantNameAndProfilePhoto.setId(source.getParticipant().getId());
+                participantNameAndProfilePhoto.setName(source.getParticipant().getName());
+
+                if(source.getParticipant().getProfilePhoto() != null) {
+                    String participantEncodedProfilePhoto = Base64.getEncoder().encodeToString(source.getParticipant()
+                            .getProfilePhoto());
+                    participantNameAndProfilePhoto.setProfilePhoto(participantEncodedProfilePhoto);
+                }
+
+
+                return new EventParticipantResponse(participantNameAndProfilePhoto, source.isEventCreator());
+            }
+        });
+
+        return modelMapper;
+    }
 
     @Bean("mergerMapper")
     public ModelMapper mergerMapper(){
