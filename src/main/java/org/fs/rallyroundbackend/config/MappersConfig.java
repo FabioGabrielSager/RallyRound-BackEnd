4b42afb19ed238adf6f1d625b19681396cb59742
shackgerc <sagerprogramer@gmail.com>
2024-04-30 10:17:29 -0300

    Add custom converters to ModelMapper Bean.
      Add custom converter to ModelMapper bean definition to map EventSchedulesEntity objects to LocalTime objects.
      Add custom converter to ModelMapper bean definition to map AddressEntity objects to AddressDto objects.

--- src/main/java/org/fs/rallyroundbackend/config/MappersConfig.java
+++ src/main/java/org/fs/rallyroundbackend/config/MappersConfig.java
@@ -3,14 +3,20 @@ package org.fs.rallyroundbackend.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.fs.rallyroundbackend.dto.event.EventParticipantResponse;
+import org.fs.rallyroundbackend.dto.location.addresses.AddressDto;
+import org.fs.rallyroundbackend.dto.location.addresses.SpecificAddressDto;
 import org.fs.rallyroundbackend.dto.participant.ParticipantResume;
 import org.fs.rallyroundbackend.entity.events.EventParticipantEntity;
+import org.fs.rallyroundbackend.entity.events.EventSchedulesEntity;
+import org.fs.rallyroundbackend.entity.location.AddressEntity;
+import org.fs.rallyroundbackend.entity.location.EntityType;
 import org.modelmapper.AbstractConverter;
 import org.modelmapper.Conditions;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalTime;
 import java.util.Base64;
 
 @Configuration
@@ -28,7 +34,7 @@ public class MappersConfig {
                 participantNameAndProfilePhoto.setId(source.getParticipant().getId());
                 participantNameAndProfilePhoto.setName(source.getParticipant().getName());
 
-                if(source.getParticipant().getProfilePhoto() != null) {
+                if (source.getParticipant().getProfilePhoto() != null) {
                     String participantEncodedProfilePhoto = Base64.getEncoder().encodeToString(source.getParticipant()
                             .getProfilePhoto());
                     participantNameAndProfilePhoto.setProfilePhoto(participantEncodedProfilePhoto);
@@ -39,11 +45,61 @@ public class MappersConfig {
             }
         });
 
+        modelMapper.addConverter(new AbstractConverter<EventSchedulesEntity, LocalTime>() {
+            @Override
+            protected LocalTime convert(EventSchedulesEntity source) {
+                return source.getSchedule().getStartingHour().toLocalTime();
+            }
+        });
+
+        modelMapper.addConverter(new AbstractConverter<AddressEntity, AddressDto>() {
+
+            @Override
+            protected AddressDto convert(AddressEntity source) {
+                return AddressDto.builder()
+                        .entityType(EntityType.Address)
+                        .address(SpecificAddressDto.builder()
+                                .houseNumber(source.getHouseNumber() != null ? source.getHouseNumber() : "")
+
+                                .countryRegion("Argentina")
+
+                                .streetName(source.getStreet().getName() != null ? source.getStreet().getName() : "")
+
+                                .neighborhood(source.getStreet().getNeighborhood().getName() != null
+                                        ? source.getStreet().getNeighborhood().getName() : "")
+
+                                .locality(source.getStreet().getNeighborhood().getLocality().getName() != null
+                                        ? source.getStreet().getNeighborhood().getLocality().getName() : "")
+
+                                .adminDistrict2(
+                                        source.getStreet().getNeighborhood().getLocality()
+                                                .getAdminSubdistrict().getName() != null
+                                                ? source.getStreet().getNeighborhood().getLocality()
+                                                        .getAdminSubdistrict().getName() : "")
+
+                                .adminDistrict(
+                                        source.getStreet().getNeighborhood().getLocality()
+                                                .getAdminSubdistrict().getAdminDistrict().getName() != null
+                                                ? source.getStreet().getNeighborhood().getLocality()
+                                                        .getAdminSubdistrict().getAdminDistrict().getName() : "")
+
+                                .postalCode(source.getPostalCode() != null ? source.getPostalCode() : "")
+
+                                .addressLine(source.getAddressLine().getAddressLine() != null
+                                        ? source.getAddressLine().getAddressLine() : "")
+
+                                .formattedAddress(source.getFormattedAddress().getFormattedAddress() != null
+                                        ? source.getFormattedAddress().getFormattedAddress() : "")
+                                .build())
+                        .build();
+            }
+        });
+
         return modelMapper;
     }
 
     @Bean("mergerMapper")
-    public ModelMapper mergerMapper(){
+    public ModelMapper mergerMapper() {
         ModelMapper mapper = new ModelMapper();
         mapper.getConfiguration().setPropertyCondition(Conditions.isNotNull());
         return mapper;
--- src/main/java/org/fs/rallyroundbackend/repository/event/EventRepository.java
+++ src/main/java/org/fs/rallyroundbackend/repository/event/EventRepository.java
@@ -2,6 +2,67 @@ package org.fs.rallyroundbackend.repository.event;
 
 import org.fs.rallyroundbackend.entity.events.EventEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-public interface EventRepository extends JpaRepository<EventEntity, Long> {
+import java.sql.Time;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface EventRepository extends JpaRepository<EventEntity, UUID> {
+    @Query("SELECT e FROM EventEntity AS e " +
+            "JOIN e.activity AS act " +
+            "JOIN e.address AS adds JOIN adds.street AS str JOIN str.neighborhood AS neigh " +
+            "JOIN neigh.locality as loc JOIN loc.adminSubdistrict as adminsd JOIN adminsd.adminDistrict AS admind " +
+            "JOIN e.eventSchedules AS es JOIN es.schedule as s " +
+            "WHERE" +
+            " (:activityName IS NULL OR act.name LIKE :activityName) " +
+            "AND (:neighborhood IS NULL OR neigh.name LIKE :neighborhood)" +
+            "AND (:locality IS NULL OR loc.name LIKE :locality)" +
+            "AND (:adminSubdistrict IS NULL OR adminsd.name LIKE :adminSubdistrict) " +
+            "AND (:adminDistrict IS NULL OR admind.name LIKE :adminDistrict) " +
+            "AND (e.date BETWEEN :dateFrom AND :dateTo) " +
+            "AND (:hours IS NULL OR s.startingHour in :hours) " +
+            "AND e.id IN (SELECT ae.id from EventEntity as ae ORDER BY ae.id LIMIT :limit OFFSET :offset) " +
+            "ORDER BY e.date DESC")
+    List<EventEntity> findAllByActivityOrAndLocationOrAndDatesOrAndHours(
+            @Param("activityName") String activityName,
+            @Param("neighborhood") String neighborhood,
+            @Param("locality") String locality,
+            @Param("adminSubdistrict") String adminSubdistrict,
+            @Param("adminDistrict") String adminDistrict,
+            @Param("dateFrom") LocalDate dateFrom,
+            @Param("dateTo") LocalDate dateTo,
+            @Param("hours") List<Time> hours,
+            @Param("limit") int limit,
+            @Param("offset") int offset
+    );
+
+
+    @Query("SELECT COUNT(DISTINCT e) FROM EventEntity AS e " +
+            "JOIN e.activity AS act " +
+            "JOIN e.address AS adds JOIN adds.street AS str JOIN str.neighborhood AS neigh " +
+            "JOIN neigh.locality as loc JOIN loc.adminSubdistrict as adminsd JOIN adminsd.adminDistrict AS admind " +
+            "JOIN e.eventSchedules AS es JOIN es.schedule as s " +
+            "WHERE" +
+            " (:activityName IS NULL OR act.name LIKE :activityName) " +
+            "AND (:neighborhood IS NULL OR neigh.name LIKE :neighborhood)" +
+            "AND (:locality IS NULL OR loc.name LIKE :locality)" +
+            "AND (:adminSubdistrict IS NULL OR adminsd.name LIKE :adminSubdistrict) " +
+            "AND (:adminDistrict IS NULL OR admind.name LIKE :adminDistrict) " +
+            "AND (e.date BETWEEN :dateFrom AND :dateTo) " +
+            "AND (:hours IS NULL OR s.startingHour in :hours)")
+    Long countAllByActivityOrAndLocationOrAndDatesOrAndHours(
+            @Param("activityName") String activityName,
+            @Param("neighborhood") String neighborhood,
+            @Param("locality") String locality,
+            @Param("adminSubdistrict") String adminSubdistrict,
+            @Param("adminDistrict") String adminDistrict,
+            @Param("dateFrom") LocalDate dateFrom,
+            @Param("dateTo") LocalDate dateTo,
+            @Param("hours") List<Time> hours
+    );
 }
