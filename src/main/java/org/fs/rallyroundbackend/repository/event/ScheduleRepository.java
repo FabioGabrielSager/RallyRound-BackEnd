2a95c40322d49cd54c1a04060ef780825f4e3151
shackgerc <sagerprogramer@gmail.com>
2024-04-30 10:15:49 -0300

    Add missing @Repository annotation.

--- src/main/java/org/fs/rallyroundbackend/repository/event/ScheduleRepository.java
+++ src/main/java/org/fs/rallyroundbackend/repository/event/ScheduleRepository.java
@@ -2,11 +2,13 @@ package org.fs.rallyroundbackend.repository.event;
 
 import org.fs.rallyroundbackend.entity.events.ScheduleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.sql.Time;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface ScheduleRepository extends JpaRepository<ScheduleEntity, UUID> {
     Optional<ScheduleEntity> findByStartingHour(Time startingHour);
 }
