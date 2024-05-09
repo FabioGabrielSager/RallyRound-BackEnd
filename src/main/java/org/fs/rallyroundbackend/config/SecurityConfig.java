dafaba9db50845d95e66f7a3a95bd782cd8ef0d1
shackgerc <sagerprogramer@gmail.com>
2024-05-09 17:53:34 -0300

    Update MercadoPagoController.
    Renaming controller.
    
    Add endpoint to capture mercado pago payments notifications.
    
    Encapsulate in a private method the logic to check mercado pago webhook events signature.
    
    Change routes configuration.

--- src/main/java/org/fs/rallyroundbackend/config/SecurityConfig.java
+++ src/main/java/org/fs/rallyroundbackend/config/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig {
                                 .requestMatchers("/rr/api/v1/mp/auth/url/").hasRole("PARTICIPANT")
                                 .requestMatchers("/rr/api/v1/mp/auth/account/linked/").hasRole("PARTICIPANT")
                                 .requestMatchers("/rr/api/v1/mp/auth/authorize/").permitAll()
+                                .requestMatchers("/rr/api/v1/mp/payment/done/").permitAll()
                                 .requestMatchers("/rr/api/v1/events/**").hasRole("PARTICIPANT")
                                 .requestMatchers("/rr/api/v1/location/autosuggest/places/{query}").permitAll()
                                 .requestMatchers("/rr/api/v1/location/autosuggest/addresses/{query}").hasRole("PARTICIPANT")
--- src/main/java/org/fs/rallyroundbackend/controller/MercadoPagoController.java
+++ src/main/java/org/fs/rallyroundbackend/controller/MercadoPagoController.java
@@ -4,16 +4,19 @@ import com.mercadopago.exceptions.MPMalformedRequestException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.codec.digest.HmacUtils;
+import org.fs.rallyroundbackend.dto.mercadopago.MpWebHookNotificationDto;
 import org.fs.rallyroundbackend.exception.mercadopago.MPAccessTokenRequestException;
 import org.fs.rallyroundbackend.exception.mercadopago.MPAccountAlreadyLinkedException;
 import org.fs.rallyroundbackend.service.JwtService;
 import org.fs.rallyroundbackend.service.MPAuthService;
+import org.fs.rallyroundbackend.service.MPPaymentService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,16 +26,17 @@ import java.util.Objects;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/rr/api/v1/mp/auth")
+@RequestMapping("/rr/api/v1/mp")
 @RequiredArgsConstructor
-public class MPAuthController {
+public class MercadoPagoController {
 
+    private final MPPaymentService mpPaymentService;
     private final MPAuthService mpAuthService;
     private final JwtService jwtService;
     @Value("${mp.hook.secret.key}")
     private String webHookKey = "";
 
-    @GetMapping("/url/")
+    @GetMapping("/auth/url/")
     public String getMpAuthUrl(HttpServletRequest request) {
         String userEmail = jwtService.getUsernameFromToken(jwtService.getTokenFromRequest(request));
         try {
@@ -42,16 +46,39 @@ public class MPAuthController {
         }
     }
 
-    @GetMapping("/account/linked/")
+    @GetMapping("/auth/account/linked/")
     public ResponseEntity<Boolean> isAccountLinked(HttpServletRequest request) {
         return ResponseEntity.ok(this.mpAuthService.isAccountAlreadyLinked(
                 this.jwtService.getUsernameFromToken(this.jwtService.getTokenFromRequest(request))));
     }
 
-    @PostMapping("authorize/")
+    @PostMapping("/auth/authorize/")
     public ResponseEntity<Boolean> linkAccount(@RequestParam(required = false) String code,
                                                @RequestParam(required = false) String state,
                                                HttpServletRequest request) {
+        this.verifyMpSignature(request);
+
+        try {
+            this.mpAuthService.getAccessToken(code, UUID.fromString(state));
+        } catch (MPAccessTokenRequestException e) {
+            throw new ResponseStatusException(HttpStatusCode.valueOf(e.getError().status()), e.getMessage());
+        }
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/payment/done/")
+    public void receivePaymentNotification(@RequestBody MpWebHookNotificationDto mpWebHookNotification,
+                                           HttpServletRequest request) {
+
+//        this.verifyMpSignature(request);
+
+        if(mpWebHookNotification.getType() == null || !mpWebHookNotification.getType().equals("payment"))
+            return;
+
+        this.mpPaymentService.processEventInscriptionPaymentNotification(mpWebHookNotification);
+    }
+
+    private void verifyMpSignature(HttpServletRequest request) {
         try {
             String xSignature = request.getHeader("x-signature");
             String xRequestId = request.getHeader("x-request-id");
@@ -80,13 +107,5 @@ public class MPAuthController {
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Signature verification failed");
         }
-
-        try {
-            this.mpAuthService.getAccessToken(code, UUID.fromString(state));
-        } catch (MPAccessTokenRequestException e) {
-            throw new ResponseStatusException(HttpStatusCode.valueOf(e.getError().status()), e.getMessage());
-        }
-
-        return ResponseEntity.ok().build();
     }
 }
