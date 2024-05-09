6403cd5e0be6ceb733c5224eb6a15c446059b2c9
shackgerc <sagerprogramer@gmail.com>
2024-05-09 16:47:34 -0300

    Update MPAuthTokenRepository to the MPAuthTokenEntity changes.

--- src/main/java/org/fs/rallyroundbackend/repository/MPAuthTokenRepository.java
+++ src/main/java/org/fs/rallyroundbackend/repository/MPAuthTokenRepository.java
@@ -4,8 +4,10 @@ import org.fs.rallyroundbackend.entity.users.participant.MPAuthTokenEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface MPAuthTokenRepository extends JpaRepository<MPAuthTokenEntity, UUID> {
+    Optional<MPAuthTokenEntity> findByUserId(Long userId);
 }
