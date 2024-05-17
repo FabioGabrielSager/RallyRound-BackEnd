65410620ae66e15c26f7e54926631b7c40589ac9
shackgerc <sagerprogramer@gmail.com>
2024-05-17 11:42:07 -0300

    Add missing @Repository annotation to UserRepository.

--- src/main/java/org/fs/rallyroundbackend/repository/user/UserRepository.java
+++ src/main/java/org/fs/rallyroundbackend/repository/user/UserRepository.java
@@ -3,10 +3,12 @@ package org.fs.rallyroundbackend.repository.user;
 import org.fs.rallyroundbackend.entity.users.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
     @Query("SELECT u FROM UserEntity AS u WHERE u.email = :email AND u.enabled = true")
     Optional<UserEntity> findEnabledUserByEmail(String email);
